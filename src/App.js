3d75c87c97b700a9e51d33d2a1695ecf48aa507b
surhokhanyan <simonyan.ars95@gmail.com>
2023-07-14 11:46:02 +0400

    lifecycle methods

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+    state = {
+        count: 0,
+        text: ""
+    }
+    constructor() {
+        super();
+        console.log("Constructor")
+    }
+
+    componentDidMount() {
+        this.setState({
+            text: "So let's start !!!"
+        })
+        console.log("Component did mount")
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log("Should component update")
+        if (nextState.count > 5)
+            return false
+        return true
+    }
+
+    componentDidUpdate() {
+        let d = new Date();
+        let text = `${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`
+        console.log("updating...", text)
+    }
+
+    increment() {
+        this.setState(curr => {
+            curr.count ++
+            return curr
+        })
+    }
+
+    render() {
+        const {count, text} = this.state;
+
+        console.log("render")
+
+        return(
+            <div>
+                <h1>Let's count!</h1>
+                <p>{text}</p>
+                <h4>You've clicked {count} times</h4>
+                <button onClick={() => this.increment()}>Up</button>
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
